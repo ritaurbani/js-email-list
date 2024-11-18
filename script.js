653783fc4cc148347f3c0f2d933036246c6b79e9
ritaurbani <ritaurbani@gmail.com>
2024-11-18 17:13:05 +0100

    feat: 10 email Array creation

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 
 
+
+let emailArray = [];
+for ( let i=0;i<10; i++) {
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
     console.log(resp);
     //preleviamo la risposta
     const emails = resp.data.response;
     console.log(emails)
+    emailArray.push(emails)
+    console.log(emailArray.length)
     
-    emailArray = [];
-    for(let i=0; i<10;i++) {
-        
-    }
-
+  
+    
 
 })
+
+}
